0d96aaa3ee38fc5f21a5bb2298fcc08d682d4338
Sean Fallmann <jorundr@hotmail.com>
2015-10-27 07:56:17 -0400

    Fixes: Images weren't uploading correctly when changing category and the image UI tweaks

--- application.py
+++ application.py
@@ -21,33 +21,49 @@ from werkzeug import secure_filename
 # http://www.clker.com/cliparts/q/L/P/Y/t/6/no-image-available-hi.png
 
 APPLICATION_NAME = "Imperial Catalog"
-APP_PATH = os.path.abspath('')
+
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.abspath('uploads/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-DB_PATH = "sqlite:///%s/catalog.db" % APP_PATH
-engine = create_engine(DB_PATH)
+engine = create_engine("sqlite:///catalog.db")
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-#  From http://flask.pocoo.org/docs/0.10/patterns/fileuploads/
+# From http://flask.pocoo.org/docs/0.10/patterns/fileuploads/
 def allowed_file(filename):
+    ''' Checks the filename to see if it has an allowed extension
+
+        The filename is checked for a '.' and everything to the left
+        of the '.' is checked against the ALLOWED_EXTENSIONS list.
+
+        The both are true, the file is an 'allowed file'.
+    '''
     return '.' in filename and \
            (filename.rsplit('.', 1)[1]).lower() in ALLOWED_EXTENSIONS
 
 
+''' Function for the catalog home page and
+    the category pages
+'''
 @app.route("/")
 @app.route("/catalog")
 @app.route("/catalog/category/<category>")
 def catalog(category=None):
 
+    ''' If a category name was passed in,
+        query the db for a category with a matching name
+        and get all the items for that category.
 
+        Otherwise, set items to the 10 newest items across
+        all categories
+
+    '''
     categories = getAllCategories()
     if category:
         category = session.query(Category).filter(Category.name == category).one()
@@ -60,108 +76,177 @@ def catalog(category=None):
                            items=items, category=category)
 
 
-@app.route("/catalog/category/<category>/<item>")
-def showItem(category=None, item=None):
+''' Function for showing specific items '''
+@app.route("/catalog/category/<category_name>/<item_name>")
+def showItem(category_name=None, item_name=None):
 
-    _category_id = (session.query(Category)
-                    .filter(Category.name == category).one()).id
-    _item = session.query(Item).filter(Item.category_id == _category_id)\
-        .filter(Item.name == item).one()
+    ''' Get the category_id from the name passed in.
+        Then get the item from the category_id and
+        the item name
+    '''
+    category_id = (session.query(Category)
+                    .filter(Category.name == category_name).one()).id
+    item = session.query(Item).filter(Item.category_id == category_id)\
+        .filter(Item.name == item_name).one()
 
-    return render_template('item.html', item=_item)
+    return render_template('item.html', item=item)
 
+
+''' Function for adding an item to the db '''
 @app.route("/catalog/additem", methods=['GET', 'POST'])
 @app.route("/catalog/<category>/additem", methods=['GET', 'POST'])
 def addItem(category=None):
 
+    ''' Set error to None.  Error will be used for checking
+        the status of operations and displaying error
+        information
+    '''
     error = None
 
+    ''' If the url had a value for category get the category '''
     if category:
         category = session.query(Category).filter(Category.name ==  category).one()
 
-    categories = session.query(Category).all()
+    ''' Set categories to all categories'''
+    categories = getAllCategories()
 
 
     if request.method == 'POST':
 
+        ''' Get the values from the form '''
         name = request.form['name']
         description = request.form['description']
         category_id = request.form['category']
 
+        ''' Check if the item name exists in the category '''
         if item_name_used(category_id, name):
+            ''' Set the error statement '''
             error = "%s exists in the selected category. Pick a different name" % name
         else:
+            ''' If the name doesn't exist add the item '''
             item = Item(name=name,description=description,category_id=category_id,user_id=1)
             session.add(item)
             session.commit()
 
+            ''' Check if the form had a file '''
             if request.files['image']:
+                ''' Set file to the upload file'''
                 file = request.files['image']
 
+                ''' Set success and error to the results of addItemFileFolder '''
                 success, error = addItemFileFolder(file, item.category_id, item.id)
 
+                ''' If it was successful, set item.image to the filename and
+                    add the item to the db again to update it
+                '''
                 if success:
                     item.image = file.filename
                     session.add(item)
                     session.commit()
+        ''' If there was an error return to add item page with the error, '''
         if error:
             return render_template('add_item.html', categories=categories, error=error)
+
         else:
-            flash('ITEM ADDED!')
-            return redirect(url_for('showItem', category=item.category.name, item=item.name))
+            ''' otherwise flash that the item wa added and go the show item page
+                with the new item's info
+            '''
+            flash('Added %s!' % item.name)
+            return redirect(url_for('showItem', category_name=item.category.name, item_name=item.name))
 
     else:
         return render_template('add_item.html', categories=categories, category=category)
 
 
-
-
+''' Function for updating an item in the db '''
 @app.route("/catalog/category/<category_name>/<item_name>/update", methods=['GET', 'POST'])
 def updateItem(category_name, item_name):
 
+    ''' Set error to None.  Error will be used for checking
+        the status of operations and displaying error
+        information
+    '''
     error = None
+
     category = session.query(Category).filter(Category.name == category_name).one()
 
+    ''' Look for the item in the db, if it's not there set
+        item to none and set the error message
+    '''
     try:
         item = session.query(Item).filter(Item.category_id == category.id).filter(Item.name == item_name).one()
     except NoResultFound:
         item = None
         error = "No item exists with this name and category"
 
-    categories = session.query(Category).all()
+
+    ''' Set categories to all categories'''
+    categories = getAllCategories()
 
     if request.method == 'POST' and item is not None:
-
+        ''' Get the values from the form '''
         name = request.form['name']
         description = request.form['description']
         category_id = request.form['category']
 
+        ''' Check if the item name exists in the category
+            and that it's not the name of the item being update.
+            Set the error message if the name is a duplicate
+        '''
         if item_name_used(category_id, name) and name != item.name:
             error = "%s exists in the selected category. Pick a different name" % name
         else:
-
+            ''' Otherwise update the values of the item '''
             item.name = name
             item.description = description
             old_category_id = item.category_id
             item.category_id = category_id
 
+            ''' Check if the form had a file '''
             if request.files['image']:
+                ''' Set file to the upload file'''
                 file = request.files['image']
 
+                ''' If the name of the new image is the
+                    same as the current image
+                    Prefix the new image filename with
+                    'copy_of_'.
+
+                    Without this step, the new file name
+                    would be deleted when cleanup occurs
+                    to remove the old file since it had the
+                    same name as the existing image.
+                '''
                 if item.image and item.image == file.filename:
                     file.filename  = "copy_of_" + file.filename
 
-                success, error = addItemFileFolder(file, item.category_id, item.id)
+                ''' Set success and error to the results of addItemFileFolder '''
+                success, error = addItemFileFolder(file, category_id, item.id)
+
+                ''' If it was successful:
+                        1. Checks if item.image has a value. If it does
+                        try removing the old file.
+
+                        2. Then if item.image has a value and the category_id
+                        of the item is different than the category_id prior to
+                        the update and file removal was successful, try removing
+                        the folder for the item's image.
+
+                        3.  If that was successful set item.image to the new filename
+                '''
+
                 if success:
                     if item.image:
-                        success, error = removeOldFile(item.category_id, item.id, item.image)
+                        success, error = removeOldFile(old_category_id, item.id, item.image)
                     elif item.image and category_id != old_category_id and success:
                         success, error = deleteItemFileFolder(old_category_id, item.id)
                     if success:
                         item.image = file.filename
 
             elif item.image and category_id != old_category_id:
-
+                ''' if the item had an image and the category changed
+                    move the item folder under the new category folder
+                '''
                 old_folder = app.config['UPLOAD_FOLDER'] + "/%s/%s/" % (old_category_id, item.id)
                 new_folder = app.config['UPLOAD_FOLDER'] + "/%s/%s/" % (category_id, item.id)
                 try:
@@ -169,10 +254,19 @@ def updateItem(category_name, item_name):
                 except OSError:
                     error = "Error moving file folder"
 
+            ''' Now the item changes can be commited to the db '''
             session.add(item)
             session.commit()
 
-    return render_template('update_item.html', categories=categories, item=item, error=error)
+        ''' If there were errors return to the update page with the errors'''
+        if error:
+            return render_template('update_item.html', categories=categories, item=item, error=error)
+        else:
+            ''' Otherwise, go the the show item page with the updated item '''
+            flash('Updated %s!' % item.name)
+            return redirect(url_for('showItem', category_name=item.category.name, item_name=item.name))
+
+    return render_template('update_item.html', categories=categories, item=item)
 
 
 @app.route("/catalog/category/<category_name>/<item_name>/delete", methods=['GET', 'POST'])
@@ -198,15 +292,16 @@ def deleteItem(category_name, item_name):
             if success:
                 item.image = None
             else:
-                return redirect(url_for('showItem', category=item.category.name, item=item.name, error=error))
+                return redirect(url_for('showItem', category_name=item.category.name, item_name=item.name, error=error))
 
         category = item.category.name
+        deleted_name = item.name
         session.delete(item)
         session.commit()
-        flash("Item deleted")
+        flash("%s deleted!" % deleted_name)
         return redirect(url_for('catalog', category=category))
     else:
-        return redirect(url_for('showItem', category=item.category.name, item=item.name, error=error))
+        return redirect(url_for('showItem', category_name=item.category.name, item_name=item.name, error=error))
 
 
 @app.route('/uploads/<category_id>/<item_id>/<filename>')
@@ -249,23 +344,24 @@ def addItemFileFolder(file, category_id, item_id):
     error = None
 
     if allowed_file(file.filename):
+
         item_folder = "/%s/%s/" % (category_id, item_id)
         folder = app.config['UPLOAD_FOLDER'] + item_folder
-
+        print item_folder, folder, file
         if not os.path.exists(folder):
-
             try:
                 os.makedirs(folder)
             except OSError:
                 error = "Error on creating file folder"
                 return success, error
 
-            try:
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(folder, filename))
-            except:
-                error = "Error on saving file"
-                return success, error
+        try:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(folder, filename))
+            print file
+        except:
+            error = "Error on saving file"
+            return success, error
 
         success = True
     else:
