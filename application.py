324ae5fcd8ae7f48535b7c375beb47387beb7f21
Sean Fallmann <jorundr@hotmail.com>
2015-10-28 00:41:29 -0400

    FIX: Working on fixing implementation of Google+ Oauth2 authentication.

--- application.py
+++ application.py
@@ -23,6 +23,10 @@ from key_gen import get_secret_key
 
 APPLICATION_NAME = "Imperial Catalog"
 
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+
+
 UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
@@ -55,13 +59,156 @@ def allowed_file(filename):
 # Create anti-forgery state token
 @app.route('/login')
 def showLogin():
-    if request.is_xhr:
-        state = get_secret_key()
-        login_session['state'] = state
-        # return "The current session state is %s" % login_session['state']
-        return render_template('login.html', STATE=state)
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+@app.route('/ajax/gconnect', methods=['POST'])
+def gconnect():
+    # Check that the value of state sent matches state stored in the session
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    print access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = answer.json()
+
+    login_session['name'] = data['name']
+    login_session['image'] = data['picture']
+    login_session['email'] = data['email']
+
+    user_id = getUserID(login_session['email'])
+
+    if user_id is None:
+        user_id = createUser(login_session)
+
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['name']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['image']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['name'])
+    return output
+
+
+# DISCONNECT - Revoke a current user's token and reset their login_session
+@app.route('/gdisconnect')
+def gdisconnect():
+        # Only disconnect a connected user.
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+
+    if result['status'] == '200':
+        # Reset the user's sesson.
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['name']
+        del login_session['email']
+        del login_session['image']
+
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
     else:
-        return redirect(url_for('catalog'))
+        # For whatever reason, the given token was invalid.
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['name'], email=login_session[
+                   'email'], image=login_session['image'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
 
 
 '''
@@ -81,6 +228,11 @@ def catalog(category=None):
         all categories
 
     '''
+    logged_in = 0
+
+    if 'email' in login_session:
+        logged_in = 1
+
     categories = getAllCategories()
     if category:
         category = session.query(Category).filter(Category.name == category).one()
@@ -88,9 +240,9 @@ def catalog(category=None):
     else:
         items = getLatestItems(10)
 
-
     return render_template('catalog.html', categories=categories,
-                           items=items, category=category)
+                               items=items, category=category, logged_in=logged_in)
+
 
 
 ''' def showItem(category_name=None, item_name=None):
@@ -99,16 +251,26 @@ def catalog(category=None):
 @app.route("/catalog/category/<category_name>/<item_name>")
 def showItem(category_name=None, item_name=None):
 
+
     ''' Get the category_id from the name passed in.
         Then get the item from the category_id and
         the item name
     '''
+
+    logged_in = None
+
     category_id = (session.query(Category)
                     .filter(Category.name == category_name).one()).id
     item = session.query(Item).filter(Item.category_id == category_id)\
         .filter(Item.name == item_name).one()
 
-    return render_template('item.html', item=item)
+    if 'email' in login_session:
+        logged_in = "logged_in"
+
+        if login_session['email'] == item.user.email:
+            return render_template('item.html', item=item, logged_in=logged_in)
+
+    return render_template('p_item.html', item=item, logged_in=logged_in)
 
 
 ''' Function for adding an item to the db '''
@@ -120,8 +282,13 @@ def addItem(category=None):
         the status of operations and displaying error
         information
     '''
-    error = None
 
+    if 'email' not in login_session:
+        return redirect(url_for("catalog"))
+
+    logged_in = "logged_in"
+    error = None
+    user_id = getUserID(login_session['email'])
     ''' If the url had a value for category get the category '''
     if category:
         category = session.query(Category).filter(Category.name ==  category).one()
@@ -143,10 +310,11 @@ def addItem(category=None):
             error = "%s exists in the selected category. Pick a different name" % name
         else:
             ''' If the name doesn't exist add the item '''
-            item = Item(name=name,description=description,category_id=category_id,user_id=1)
+            item = Item(name=name,description=description,category_id=category_id,user_id=user_id)
+
             session.add(item)
             session.commit()
-
+            print  item.name, item.category.name
             ''' Check if the form had a file '''
             if request.files['image']:
                 ''' Set file to the upload file'''
@@ -174,7 +342,7 @@ def addItem(category=None):
             return redirect(url_for('showItem', category_name=item.category.name, item_name=item.name))
 
     else:
-        return render_template('add_item.html', categories=categories, category=category)
+        return render_template('add_item.html', categories=categories, category=category, logged_in=logged_in)
 
 
 ''' Function for updating an item in the db '''
@@ -185,6 +353,11 @@ def updateItem(category_name, item_name):
         the status of operations and displaying error
         information
     '''
+    if 'email' not in login_session:
+        return redirect(url_for("catalog"))
+
+
+
     error = None
 
     category = session.query(Category).filter(Category.name == category_name).one()
@@ -197,6 +370,12 @@ def updateItem(category_name, item_name):
     except NoResultFound:
         item = None
         error = "No item exists with this name and category"
+
+    logged_in = "logged_in"
+    print "l_s", login_session['email'], item.user.email
+    if login_session["email"] != item.user.email:
+
+        return redirect(url_for("catalog"))
 
 
     ''' Set categories to all categories'''
